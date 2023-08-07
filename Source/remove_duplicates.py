6b697d97f97b51044b28a1cd7aff15d2296eef48
Luciano <lucianogpingindustrial@gmail.com>
2023-08-07 16:40:43 +0200

    feffd

--- Source/remove_duplicates.py
+++ Source/remove_duplicates.py
@@ -2,25 +2,24 @@ import os
 from PIL import Image
 import imagehash
 
-def remove_duplicates(directory):
-    image_hashes = {}
-    
+directory_path = ""
+target_files = ".png, .jpg"
+
+def remove_duplicates(directory, target_files):
+    image_hashes = {}    
     # Iterate over all files in the directory
     for filename in os.listdir(directory):
-        file_path = os.path.join(directory, filename)
-        
+        file_path = os.path.join(directory, filename)        
         # Skip non-image files
         if not os.path.isfile(file_path) or not file_path.lower().endswith(target_files):
-            continue
-        
+            continue        
         try:
             # Calculate the hash value of the image
             with Image.open(file_path) as img:
                 hash_value = imagehash.average_hash(img)
         except (OSError, Image.UnidentifiedImageError):
             # Ignore files that cannot be opened as images
-            continue
-        
+            continue        
         # Check if the hash value already exists
         if hash_value in image_hashes:
             # Remove the duplicate image
@@ -31,4 +30,4 @@ def remove_duplicates(directory):
             image_hashes[hash_value] = file_path
 
 # Provide the path to the directory containing the pictures
-remove_duplicates(directory_path)
+remove_duplicates(directory_path, target_files)
